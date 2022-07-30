0d7d195cdef1235d48efece26247378d9ac397bb
Brian Alegre <brialegre@yahoo.com>
2022-07-29 21:16:13 -0700

    Fixed issue with seed data

--- utils/seed.js
+++ utils/seed.js
@@ -5,20 +5,20 @@ const { User, Thought } = require("../models");
 // USER SEED DATA
 const userSeed = [
     {
-        'username': 'userOne',
-        'email': 'userOne@gmail.com',
+        username: 'userOne',
+        email: 'userOne@gmail.com',
     },
     {
-        'username': 'userTwo',
-        'email': 'userTwo@gmail.com',
+        username: 'userTwo',
+        email: 'userTwo@gmail.com',
     },
     {
-        'username': 'userThree',
-        'email': 'userThree@gmail.com',
+        username: 'userThree',
+        email: 'userThree@gmail.com',
     },
     {
-        'username': 'userFour',
-        'email': 'userFour@gmail.com',
+        username: 'userFour',
+        email: 'userFour@gmail.com',
     },
 ]
 
